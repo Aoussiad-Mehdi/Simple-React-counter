7295991cf26a4f8eb81e7f7baa8d497eab299eea
Aoussiad-Mehdi <mehdiouss315@gmail.com>
2020-12-17 23:39:02 +0100

    simple counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React , { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+// Our function component.
+const Component = () =>{
+  const [value, setValue] = useState(0);
+  const setToZero = ()=>{
+    setValue(0);
+  }
+  return(
+    <div className="container">
+      <h1>Counter</h1>
+      <h2>{value}</h2>
+      <div className="buttons">
+        <button onClick={()=> setValue(value + 1)}>Increase</button>
+        <button onClick={setToZero}>Reset</button>
+        <button onClick={()=> setValue(value - 1)}>Decrease</button>
+      </div>
+    </div>
+  )
+}
+
+// Rendering the component with the render method.
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Component />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
